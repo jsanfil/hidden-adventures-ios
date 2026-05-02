052af09a596441f418bd6e1ea1dcc86fdc049f5d
Joe Sanfilippo <joe.sanfilippo@gmail.com>
2026-05-02 11:27:23 -0700

    test: add invite copy seam coverage

---
+++ Tests/InviteFriendsScreenModelTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import HiddenAdventures
+
+final class InviteFriendsScreenModelTests: XCTestCase {
+  func testInviteMessageIncludesAppNameAndURL() {
+    let url = URL(string: "https://hiddenadventures.app/invite")!
+
+    let message = InviteFriendsCopy.inviteMessage(appURL: url)
+
+    XCTAssertTrue(message.contains("Hidden Adventures"))
+    XCTAssertTrue(message.contains(url.absoluteString))
+  }
+
+  func testFixtureInviteMessageMatchesInviteCopyHelper() {
+    XCTAssertEqual(
+      MockFixtures.inviteMessage,
+      InviteFriendsCopy.inviteMessage(appURL: MockFixtures.inviteAppURL)
+    )
+  }
+}
